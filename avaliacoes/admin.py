cf9cf09d86b7db4d2cd57bbe88d08dcc4b31c102
Romope83 <ro007ny@hotmail.com>
2025-11-13 19:49:50 -0300

    funcionando

--- avaliacoes/admin.py
+++ avaliacoes/admin.py
@@ -1,7 +1,19 @@
+# Em avaliacoes/admin.py
+
 from django.contrib import admin
-from .models import Professor, Avaliacao, Materia, Aluno
+# IMPORTAÇÕES ADICIONADAS:
+from .models import (
+    Professor, Avaliacao, Materia, Aluno, DisciplinaPessoa, 
+    Categoria, AvaliacaoCategoria
+)
 
 admin.site.register(Professor)
 admin.site.register(Avaliacao)
 admin.site.register(Materia)
 admin.site.register(Aluno)
+admin.site.register(DisciplinaPessoa) # Você já deve ter feito isso
+
+# --- ADICIONE ESTAS LINHAS ---
+admin.site.register(Categoria)
+admin.site.register(AvaliacaoCategoria)
+# --- FIM DA ADIÇÃO ---
