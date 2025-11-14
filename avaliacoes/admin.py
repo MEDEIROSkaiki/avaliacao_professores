7a91d6aee4c71df358cb037b37aa3f3203ae8822
Kaua <kaua.fmelo@hotmail.com>
2025-11-14 19:55:53 -0300

    u

--- avaliacoes/admin.py
+++ avaliacoes/admin.py
@@ -1,19 +1,39 @@
-# Em avaliacoes/admin.py
-
 from django.contrib import admin
-# IMPORTAÇÕES ADICIONADAS:
+from django.contrib.auth.admin import UserAdmin
 from .models import (
-    Professor, Avaliacao, Materia, Aluno, DisciplinaPessoa, 
-    Categoria, AvaliacaoCategoria
+    CustomUser, Professor, Aluno, Materia, DisciplinaPessoa, 
+    Categoria, Avaliacao, AvaliacaoCategoria
 )
 
-admin.site.register(Professor)
-admin.site.register(Avaliacao)
-admin.site.register(Materia)
-admin.site.register(Aluno)
-admin.site.register(DisciplinaPessoa) # Você já deve ter feito isso
+# Esta classe diz ao Admin como mostrar seu CustomUser
+class CustomUserAdmin(UserAdmin):
+    # Copia os campos do UserAdmin padrão e adiciona os seus
+    fieldsets = UserAdmin.fieldsets + (
+        ('Campos Personalizados', {
+            'fields': ('user_type', 'cpf', 'data_nascimento'),
+        }),
+    )
+    
+    # Adiciona seus campos na tela de criação de usuário
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Campos Personalizados', {
+            'fields': ('user_type', 'cpf', 'data_nascimento', 'first_name', 'last_name', 'email'),
+        }),
+    )
+    
+    # Adiciona 'user_type' na lista de usuários
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'user_type')
 
-# --- ADICIONE ESTAS LINHAS ---
+# --- REGISTROS ---
+
+# Registra seu CustomUser com a classe personalizada
+admin.site.register(CustomUser, CustomUserAdmin)
+
+# Registra todos os seus outros modelos (alguns podem já estar aqui)
+admin.site.register(Professor)
+admin.site.register(Aluno)
+admin.site.register(Materia)
+admin.site.register(DisciplinaPessoa)
 admin.site.register(Categoria)
+admin.site.register(Avaliacao)
 admin.site.register(AvaliacaoCategoria)
-# --- FIM DA ADIÇÃO ---
